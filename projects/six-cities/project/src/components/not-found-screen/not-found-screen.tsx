bf2c58a4ac0e90bc06c6c8e9bdf0405c86797598
mi5ha6in <mi5ha6in@gmail.com>
2022-06-09 22:16:58 +0300

    adds NotFoundScreen component

---
+++ projects/six-cities/project/src/components/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,10 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <>
+      <h1>404. Page not found</h1>
+      <a href="/">Вернуться на главную</a>
+    </>
+  );
+}
+
+export default NotFoundScreen;
