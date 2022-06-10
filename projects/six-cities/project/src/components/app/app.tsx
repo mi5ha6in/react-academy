d9081e477a8d1e09f0918061f2bec066d04f4de0
mi5ha6in <mi5ha6in@gmail.com>
2022-06-10 14:41:35 +0300

    adds router

--- projects/six-cities/project/src/components/app/app.tsx
+++ projects/six-cities/project/src/components/app/app.tsx
@@ -1,11 +1,33 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreen from '../property-screen/property-screen';
+
+import { AppRoute } from '../../const';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 type AppProps = {
-  countPlaceCard: number
-}
+  countPlaceCard: number;
+};
 
-function App({countPlaceCard}: AppProps): JSX.Element {
-  return <MainScreen countPlaceCard={countPlaceCard} />;
+function App({ countPlaceCard }: AppProps): JSX.Element {
+  // return <MainScreen countPlaceCard={countPlaceCard} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen countPlaceCard={countPlaceCard} />}
+        />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route path={AppRoute.Room} element={<PropertyScreen />} />
+        <Route path="*" element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
