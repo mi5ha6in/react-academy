afb95b441cdfdde1aa668e281cb973752754acd2
mi5ha6in <mi5ha6in@gmail.com>
2022-06-10 16:55:19 +0300

    changes Route on PrivateRoute

--- projects/six-cities/project/src/components/app/app.tsx
+++ projects/six-cities/project/src/components/app/app.tsx
@@ -4,16 +4,16 @@ import MainScreen from '../main-screen/main-screen';
 import LoginScreen from '../login-screen/login-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import PropertyScreen from '../property-screen/property-screen';
-
-import { AppRoute } from '../../const';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   countPlaceCard: number;
 };
 
 function App({ countPlaceCard }: AppProps): JSX.Element {
-  // return <MainScreen countPlaceCard={countPlaceCard} />;
   return (
     <BrowserRouter>
       <Routes>
@@ -22,7 +22,14 @@ function App({ countPlaceCard }: AppProps): JSX.Element {
           element={<MainScreen countPlaceCard={countPlaceCard} />}
         />
         <Route path={AppRoute.Login} element={<LoginScreen />} />
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
         <Route path={AppRoute.Room} element={<PropertyScreen />} />
         <Route path="*" element={<NotFoundScreen />} />
       </Routes>
